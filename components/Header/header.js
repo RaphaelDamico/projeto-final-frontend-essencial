dd161c2120351985562ac874ca082bb0642106ec
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-04-30 17:53:05 -0300

    push: alteração no header.js

--- components/Header/header.js
+++ components/Header/header.js
@@ -1,16 +1,16 @@
 function Header() {
     const header = document.createElement('header');
     header.innerHTML = `
-    <header id="navbar" class="headerContainer">
-        <div class="headerContent">
-            <img class="logo-pagina" src="/Home/assets/logo-serra-tour.png" alt="logo da pagina" />
-            <nav>
-                <a class="link-nav" href="/Home/home.html">HOME</a>
-                <a class="link-nav" href="/Passeios/passeios.html">PASSEIOS</a>
-                <a class="link-nav" href="/QuemSomos/quemSomos.html">QUEM SOMOS</a>
-            </nav>
+        <div id="navbar" class="headerContainer">
+            <div class="headerContent">
+                <img class="logo-pagina" src="/Home/assets/logo-serra-tour.png" alt="logo da pagina" />
+                <nav>
+                    <a class="link-nav" href="/Home/home.html">HOME</a>
+                    <a class="link-nav" href="/Passeios/passeios.html">PASSEIOS</a>
+                    <a class="link-nav" href="/QuemSomos/quemSomos.html">QUEM SOMOS</a>
+                </nav>
+            </div>
         </div>
-    </header>
     `;
     return header;
 }
