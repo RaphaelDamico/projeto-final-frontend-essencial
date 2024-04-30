a5605e64fb50b7288ff111b585d6aceb767d2d62
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-04-29 22:03:49 -0300

    push: alterando header e ajustando pagina quem somos e home

--- components/Header/header.js
+++ components/Header/header.js
@@ -1,13 +1,16 @@
 function Header() {
     const header = document.createElement('header');
     header.innerHTML = `
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet"> 
     <header class="headerContainer">
         <div class="headerContent">
-            <img src="/Home/assets/logo-serra-tour.png" alt="logo da pagina" />
+            <img class="logo-pagina" src="/Home/assets/logo-serra-tour.png" alt="logo da pagina" />
             <nav>
-                <a class="link-nav" href="/Home/home.html">Home</a>
-                <a class="link-nav" href="/Passeios/passeios.html">Passeios</a>
-                <a class="link-nav" href="/QuemSomos/quemSomos.html">Quem somos</a>
+                <a class="link-nav" href="/Home/home.html">HOME</a>
+                <a class="link-nav" href="/Passeios/passeios.html">PASSEIOS</a>
+                <a class="link-nav" href="/QuemSomos/quemSomos.html">QUEM SOMOS</a>
             </nav>
         </div>
     </header>
@@ -20,3 +23,5 @@ function mostraHeader() {
     const body = document.querySelector('body');
     body.insertBefore(header, body.firstChild);
 }
+
+
