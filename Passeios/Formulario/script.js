8bb00b5aa34cd046e4291380be892125684fb542
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-05-03 09:50:50 -0300

    push: detalhes de indentação na pagina quem somos | implementação de funções a pagina de formulários

--- Passeios/Formulario/script.js
+++ Passeios/Formulario/script.js
@@ -1,6 +1,7 @@
 const formEl = document.getElementById('form-api');
         formEl.addEventListener('submit', e => {
             e.preventDefault();
+            
             const formData = new FormData(formEl);
             const data = Object.fromEntries(formData);
             fetch('http://localhost:8080/dados-do-cliente', {
@@ -9,5 +10,7 @@ const formEl = document.getElementById('form-api');
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(data)
-            }).then(res => res.json()).then(data => console.log(data))
+            }).then(res => res.json()).then(data => console.log(data));
+            window.location.href = 'http://127.0.0.1:5500/Home/home.html';
+            alert('Formulário enviado com sucesso! Por favor, verifique seu e-mail.')
         });
